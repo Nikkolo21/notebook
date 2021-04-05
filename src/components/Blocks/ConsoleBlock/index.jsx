488c86d68b247cbcba1e396add70c29b125bdb8b
Luis Machillanda <nikko1801@gmail.com>
2021-04-04 19:27:53 -0500

    code block styling

--- src/components/Blocks/ConsoleBlock/index.jsx
+++ src/components/Blocks/ConsoleBlock/index.jsx
@@ -5,7 +5,7 @@ const ConsoleBlock = ({ info }) => {
   return (
     <Playground
       title={info?.id}
-      style={{marginTop: 20, padding: 8, backgroundColor: "rgba(237, 246, 255, 1"}}
+      style={{marginTop: 10, paddingLeft: 6, backgroundColor: "rgba(237, 246, 255, 1"}}
       panes={[{type: 'player', style: {display: 'none'}}, {type: 'console', showLineNumber: false, renderReactElements: true}]}
       code={info?.result}
     />
